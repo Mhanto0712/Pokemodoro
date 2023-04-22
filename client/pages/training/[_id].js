02565847f81ef8abebbbd993dcbe5bf903716495
Nelson Liou <mhanto0712@gmail.com>
2023-04-22 16:23:08 +0800

    Edit cookie setting

--- client/pages/training/[_id].js
+++ client/pages/training/[_id].js
@@ -655,7 +655,7 @@ export default function Training({
       document.cookie =
         "LastTrainingPokemon=" +
         data.id +
-        "; path=/; max-age=2592000; domain=.zeabur.app ;sameSite=None ;secure=true";
+        "; path=/; max-age=2592000; domain=.zeabur.app ;SameSite=None ;Secure=true";
       let checkDocument = await PokeService.checkDocument(realToken);
       if (checkDocument.data == "unStored") {
         await PokeService.addNewDocument(data.id, 0, 0, 0, realToken);
